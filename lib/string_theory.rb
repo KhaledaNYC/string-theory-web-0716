ab25fea7dae5d411fb5e3f3c3ce3b0134a979234
Khaleda Khan <KhaledaNYC@Khaledas-MacBook.local>
2016-11-02 19:05:24 -0400

    sorting

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,14 @@
 def sort_by_appearance(characters_to_sort, long_text)
-  # code goes here
+  char_obj = {}
+  characters_to_sort.split('').each do |letter|
+  	if !long_text.include?(letter)
+  		char_obj[letter] = 0
+  	end
+  end
+  long_text.split('').each do |char|
+    if characters_to_sort.include?(char)
+      char_obj[char] ? char_obj[char] += 1 : char_obj[char] = 1
+    end
+  end
+  char_obj.sort_by{|k,v| v}.reverse.map { |c| c[0] }.join
 end
